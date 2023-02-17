3572ba4eb0fd2b5d8ded190a8bd3c33f6bdfb512
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-02-17 09:27:05 -0500

    Updated NEWS and documented the package

--- R/HubBSP2-package.R
+++ R/HubBSP2-package.R
@@ -1,9 +1,12 @@
-#' Title
-#' 
-#' @description
+#' Provide access to BrainSEQ Phase II processed data objects.
+#'
+#' @description This is an example ExperimentHub package made live during an LIBD
+#'    rstats club session. It provides access to the BrainSEQ Phase II
+#'    RangedSummarizedExperiment object at the gene-level. See
+#'    http://eqtl.brainseq.org/phase2/ for more information.
 #'
 #' @name HubBSP2-package
 #' @aliases HubBSP2-package HubBSP2
 #' @docType package
-#' @author
+#' @author Leonardo Collado-Torres
 NULL
