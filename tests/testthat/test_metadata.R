504dc7ec82c85f7910cc9a782650dc454e2ae2d9
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-03-10 15:31:35 -0500

    Make changes matching what we described at https://github.com/Bioconductor/HubPub/issues/7

--- tests/testthat/test_metadata.R
+++ tests/testthat/test_metadata.R
@@ -5,7 +5,7 @@ test_that("metadata is valid",
     if(!requireNamespace("AnnotationHubData", quietly = TRUE))
         BiocManager::install("AnnotationHubData")
 
-    path <- find.package("")
-    metadata <- system.file("extdata", "metadata.csv", package = "HubBSP2")
-    expect_true(AnnotationHubData::makeAnnotationHubMetadata(path, metadata))
+    path <- find.package("HubBSP2")
+    metadata <- "metadata.csv"
+    expect_type(AnnotationHubData::makeAnnotationHubMetadata(path, metadata), "list")
 })
